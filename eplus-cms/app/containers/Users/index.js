f031cdc1ac74d0883aa279f757238a89ac454081
unknown <pmduc1997@gmail.com>
2019-11-14 11:42:55 +0700

    minor

--- eplus-cms/app/containers/Users/index.js
+++ eplus-cms/app/containers/Users/index.js
@@ -1,11 +1,66 @@
 import React, { Component } from 'react';
+import { Table } from 'antd';
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        sorter: true,
+        width: '20%',
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        width: '20%',
+    },
+    {
+        title: 'Email',
+        dataIndex: 'email',
+    },
+];
 
 export default class Users extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [
+                {
+                  key: '1',
+                  name: 'John Brown',
+                  age: 32,
+                  email: 'godofciv6@gmail.com',
+                },
+                {
+                  key: '2',
+                  name: 'Jim Green',
+                  age: 42,
+                  email: 'godofciv6@gmail.com',
+                },
+                {
+                  key: '3',
+                  name: 'Joe Black',
+                  age: 32,
+                  email: 'godofciv6@gmail.com',
+                },
+                {
+                  key: '4',
+                  name: 'Jim Red',
+                  age: 32,
+                  email: 'godofciv6@gmail.com',
+                },
+              ],
+            pagination: {},
+            loading: false,
+        }
+    }
     render() {
         return (
-            <h1>
-                Test
-          </h1>
+            <Table
+                columns={columns}
+                rowKey={record => record.key}
+                dataSource={this.state.data}
+                pagination={this.state.pagination}
+                loading={this.state.loading}
+            />
         );
     }
 }
